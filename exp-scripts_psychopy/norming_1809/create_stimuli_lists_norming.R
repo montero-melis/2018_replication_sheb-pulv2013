9eb1dde7ae657cd36463d31922b597b5139d933a
montero-melis <montero.gui@gmail.com>
2018-09-04 14:35:27 +0200

    Processes list of Swedish verbs for norming and saves it to disk

--- exp-scripts_psychopy/norming_1809/create_stimuli_lists_norming.R
+++ exp-scripts_psychopy/norming_1809/create_stimuli_lists_norming.R
@@ -1,7 +1,12 @@
-## Create stimuli lists from the more detailed list of verbs to be normed
-## (downloaded from Google Drive)
+## Create stimuli lists from the more detailed lists of verbs that need to be
+## normed (downloaded from Google Drive)
 
 getwd()
+
+
+#  ------------------------------------------------------------------------
+#  English verbs
+#  ------------------------------------------------------------------------
 
 norming <- read.csv("exp-scripts_psychopy/norming_1809/verbs_norming_eng_sept2019_detailed.csv")
 head(norming)
@@ -14,5 +19,23 @@ sum(table(norming_simple$word) != 1)  # Check no repeated verbs (value has to be
 
 write.csv(norming_simple,
           "exp-scripts_psychopy/norming_1809/verbs_norming_eng_sept2019_simple.csv", 
+          row.names = FALSE, fileEncoding = "UTF-8")
+
+#  ------------------------------------------------------------------------
+#  Swedish verbs
+#  ------------------------------------------------------------------------
+
+norming_sw <- read.csv("exp-scripts_psychopy/norming_1809/verbs_norming_swe_sept2019_detailed.csv",
+                       fileEncoding = "UTF-8")
+head(norming_sw)
+
+norming_sw_simple <- data.frame(word = toupper(as.character(norming_sw$word)))
+norming_sw_simple$word <- trimws(norming_sw_simple$word)
+head(norming_sw_simple)
+
+sum(table(norming_sw_simple$word) != 1)  # Check no repeated verbs (value has to be zero!)
+
+write.csv(norming_sw_simple,
+          "exp-scripts_psychopy/norming_1809/verbs_norming_swe_sept2019_simple.csv", 
           row.names = FALSE, fileEncoding = "UTF-8")
 
