e5ba7a8c4062b1650bf1dea970cdc1a157faa980
montero-melis <montero.gui@gmail.com>
2018-09-06 15:32:15 +0200

    adds R script for power analysis for pilot study with natives

---
+++ power-analysis/power-analysis_pilot_natives.R
@@ -0,0 +1,14 @@
+## Power analysis for pilot study: a conceptual replication of Shebani & 
+## Pulvermüller (2013, Cortex), henceforth S&P
+
+# We will try to replicate the effect reported in that study, whereby there
+# was an interaction of WordType (arm vs leg verb) and MovementType (arm vs
+# leg movements), leading to worse memory for arm than for leg verbs when
+# performing arm movements and worse memory for leg than for arm verbs when
+# performing leg movements.
+
+
+# Estimates ---------------------------------------------------------------
+
+# Base estimates on S&P
+
