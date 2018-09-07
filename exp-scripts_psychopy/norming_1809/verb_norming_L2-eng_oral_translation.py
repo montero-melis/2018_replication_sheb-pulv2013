02873126e08f3625163f79fb15e82c6b61a6c912
piajarnefelt <piajarnefelt@gmail.com>
2018-09-07 14:27:46 +0200

    Changes experiment name that appears on data files
    Changed under settings in Builder. Note this is what matters for how
    data files are called, and not the actual name of the experiment script.

--- exp-scripts_psychopy/norming_1809/verb_norming_L2-eng_oral_translation.py
+++ exp-scripts_psychopy/norming_1809/verb_norming_L2-eng_oral_translation.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy2 Experiment Builder (v1.90.1),
-    on september 07, 2018, at 14:22
+    on september 07, 2018, at 14:23
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
@@ -26,7 +26,7 @@ _thisDir = os.path.dirname(os.path.abspath(__file__)).decode(sys.getfilesystemen
 os.chdir(_thisDir)
 
 # Store info about the experiment session
-expName = u'verb_norming_L1-swe_no_translation'  # from the Builder filename that created this script
+expName = u'verb_norming_L2-eng_oral_translation'  # from the Builder filename that created this script
 expInfo = {u'session': u'001', u'participant': u''}
 dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
 if dlg.OK == False:
@@ -79,13 +79,13 @@ pptID = int(expInfo['participant'])  # participant ID
 
 # alternate between participants whether legs or arms ratings come first
 if pptID % 2 == 0: # if even participant number
-    armlegorder_file = 'arm_first_swe.csv'
-    firstrating = u"armar/händer"
-    secondrating = u"ben/fötter"
+    armlegorder_file = 'arm_first.csv'
+    firstrating = "hand/arm"
+    secondrating = "foot/leg"
 else:
-    armlegorder_file = 'leg_first_swe.csv'
-    firstrating = u"ben/fötter"
-    secondrating = u"armar/händer"
+    armlegorder_file = 'leg_first.csv'
+    firstrating = "foot/leg"
+    secondrating = "hand/arm"
 
 # trial loop loads a random list for the translation task
 # define its path here
@@ -93,11 +93,9 @@ else:
 #list_norming_task = path2stimuli + `pptID` + '_norming.csv'
 
 ## Other
-cont = u"\n\nTryck på mellanslag för att fortsätta"  # At the end of instruction slide
-end = ""  # Needed for final slide
-
+cont = "\n\nPress space bar to continue"  # At the end of instruction slide
 introtext = visual.TextStim(win=win, name='introtext',
-    text=u"Verbassocieringsuppgift \n\nVälkommen och tack för att du tar dig tid att göra det här experimentet!\nDin uppgift är att rangordna verb enligt två olika skalor."+ cont,
+    text="Word Association Task \n\nWelcome and thank you for taking the time to participate!\nIn this experiment you will perform two tasks: a rating task and a translation task."+ cont,
     font='Arial',
     pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1,
@@ -106,7 +104,7 @@ introtext = visual.TextStim(win=win, name='introtext',
 # Initialize components for Routine "instr_1"
 instr_1Clock = core.Clock()
 text = visual.TextStim(win=win, name='text',
-    text=u"Du kommer att läsa en rad olika svenska verb. För varje verb ska du ange hur mycket du förknippar handlingen som verbet uttrycker med armar/händer och med ben/fötter. Till exempel uttrycker verbet SKRIVA en handling som är starkt förknippad med armar/händer men inte med ben/fötter. Verbet SPARKA å andra sidan uttrycker en handling som starkt förknippas med ben/fötter, men inte med armar/händer."+ cont,
+    text="You will read a series of English verbs. For each word, you will be asked to indicate to what extent you associate the action expressed by the verb with your feet/legs and your hands/arms. For example, the action denoted by the verb WRITE is strongly associated with your hands/arms, but not with your feet/legs. A verb like KICK, on the other hand, denotes an action that is strongly related to your feet/legs but not to your hands/arms." +cont,
     font='Arial',
     pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1,
@@ -115,7 +113,18 @@ text = visual.TextStim(win=win, name='text',
 # Initialize components for Routine "instr_2"
 instr_2Clock = core.Clock()
 text_2 = visual.TextStim(win=win, name='text_2',
-    text=u"Du ska bedöma varje verb två gånger. Först ska du ange hur starkt du förknippar det med "+firstrating + u", och sedan ska du ange hur starkt du förknippar det med "+secondrating + u". Använd siffertangenterna 1 till 7 för att göra ditt val. När du har bestämt dig trycker du på enter."+cont,
+    text="You will rate each verb twice: once for its " + firstrating + "-relatedness and once for its " +secondrating+ "-relatedness. Use the number keys 1 to 7 to indicate your rating. Lock in your answer by pressing return."+cont
+,
+    font='Arial',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0);
+
+# Initialize components for Routine "instr_3"
+instr_3Clock = core.Clock()
+text_3 = visual.TextStim(win=win, name='text_3',
+    text="After the ratings, you will be asked to translate the verb into Swedish. Your voice will be recorded, so simply say the translation out loud. If you don't know the verb, give your best guess.\n\nSome of the words also work as nouns, and mean something else when they are not verbs. Please keep in mind that it is the VERB you should translate. Once you have given your translation, press return to go to the next verb." +cont
+,
     font='Arial',
     pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1,
@@ -124,7 +133,7 @@ text_2 = visual.TextStim(win=win, name='text_2',
 # Initialize components for Routine "instr_4"
 instr_4Clock = core.Clock()
 text_4 = visual.TextStim(win=win, name='text_4',
-    text=u"Om du har några frågor kan du ställa dem till experimentledaren nu.\n\nÄr du redo att börja?" +cont,
+    text="If you have questions so far, you may ask the experimenter now.\n\nAre you ready to start?" +cont,
     font='Arial',
     pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1,
@@ -147,12 +156,21 @@ text_10 = visual.TextStim(win=win, name='text_10',
     pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1,
     depth=0.0);
-rating = visual.RatingScale(win=win, name='rating', marker='triangle', size=1.0, pos=[0.0, -0.4], low=1, high=7, labels=['1', '7'], scale='Inte alls                                                        Mycket starkt')
+rating = visual.RatingScale(win=win, name='rating', marker='triangle', size=1.0, pos=[0.0, -0.4], low=1, high=7, labels=['1', '7'], scale='Not at all related                                     Strongly related')
 
 # Initialize components for Routine "blank"
 blankClock = core.Clock()
 short_blank = visual.TextStim(win=win, name='short_blank',
     text=None,
+    font='Arial',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0);
+
+# Initialize components for Routine "translation"
+translationClock = core.Clock()
+text_7 = visual.TextStim(win=win, name='text_7',
+    text='default text',
     font='Arial',
     pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1,
@@ -170,7 +188,7 @@ text_5 = visual.TextStim(win=win, name='text_5',
 # Initialize components for Routine "end_of_experiment"
 end_of_experimentClock = core.Clock()
 text_9 = visual.TextStim(win=win, name='text_9',
-    text=u"Nu är experimentet slut.\n\nTack för att du deltagit!" + end,
+    text='This is the end of this experiment.\n\n\nThank you for participating!',
     font='Arial',
     pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1,
@@ -499,6 +517,85 @@ thisExp.nextEntry()
 # the Routine "instr_2" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
 
+# ------Prepare to start Routine "instr_3"-------
+t = 0
+instr_3Clock.reset()  # clock
+frameN = -1
+continueRoutine = True
+# update component parameters for each repeat
+key_resp_6 = event.BuilderKeyResponse()
+# keep track of which components have finished
+instr_3Components = [text_3, key_resp_6]
+for thisComponent in instr_3Components:
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+
+# -------Start Routine "instr_3"-------
+while continueRoutine:
+    # get current time
+    t = instr_3Clock.getTime()
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *text_3* updates
+    if t >= 0.0 and text_3.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        text_3.tStart = t
+        text_3.frameNStart = frameN  # exact frame index
+        text_3.setAutoDraw(True)
+    
+    # *key_resp_6* updates
+    if t >= 0.0 and key_resp_6.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        key_resp_6.tStart = t
+        key_resp_6.frameNStart = frameN  # exact frame index
+        key_resp_6.status = STARTED
+        # keyboard checking is just starting
+        win.callOnFlip(key_resp_6.clock.reset)  # t=0 on next screen flip
+        event.clearEvents(eventType='keyboard')
+    if key_resp_6.status == STARTED:
+        theseKeys = event.getKeys(keyList=['space'])
+        
+        # check for quit:
+        if "escape" in theseKeys:
+            endExpNow = True
+        if len(theseKeys) > 0:  # at least one key was pressed
+            key_resp_6.keys = theseKeys[-1]  # just the last key pressed
+            key_resp_6.rt = key_resp_6.clock.getTime()
+            # a response ends the routine
+            continueRoutine = False
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in instr_3Components:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # check for quit (the Esc key)
+    if endExpNow or event.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "instr_3"-------
+for thisComponent in instr_3Components:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# check responses
+if key_resp_6.keys in ['', [], None]:  # No response was made
+    key_resp_6.keys=None
+thisExp.addData('key_resp_6.keys',key_resp_6.keys)
+if key_resp_6.keys != None:  # we had a response
+    thisExp.addData('key_resp_6.rt', key_resp_6.rt)
+thisExp.nextEntry()
+# the Routine "instr_3" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+
 # ------Prepare to start Routine "instr_4"-------
 t = 0
 instr_4Clock.reset()  # clock
@@ -633,7 +730,7 @@ for thisComponent in blank_longComponents:
 # set up handler to look after randomisation of conditions etc
 trials = data.TrialHandler(nReps=1, method='random', 
     extraInfo=expInfo, originPath=-1,
-    trialList=data.importConditions('verbs_norming_swe_sept2019_simple.csv'),
+    trialList=data.importConditions('verbs_norming_eng_sept2019_simple.csv'),
     seed=None, name='trials')
 thisExp.addLoop(trials)  # add the loop to the experiment
 thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
@@ -674,7 +771,7 @@ for thisTrial in trials:
         frameN = -1
         continueRoutine = True
         # update component parameters for each repeat
-        text_10.setText(category + u"\n\nHur starkt förknippar du verbet "+ word + " med " + category_lowercase + "?")
+        text_10.setText(category + "-relatedness\n\nHow " + category_lowercase + "-related is the verb " + word + "?")
         rating.reset()
         # keep track of which components have finished
         rating_trialComponents = [text_10, rating]
@@ -785,6 +882,85 @@ for thisTrial in trials:
         
     # completed 1 repeats of 'armleg_loop'
     
+    
+    # ------Prepare to start Routine "translation"-------
+    t = 0
+    translationClock.reset()  # clock
+    frameN = -1
+    continueRoutine = True
+    # update component parameters for each repeat
+    text_7.setText("What does TO " +word+ " mean ?\nSay your Swedish translation out loud.\n\n\nPress return to continue" )
+    key_resp_7 = event.BuilderKeyResponse()
+    # keep track of which components have finished
+    translationComponents = [text_7, key_resp_7]
+    for thisComponent in translationComponents:
+        if hasattr(thisComponent, 'status'):
+            thisComponent.status = NOT_STARTED
+    
+    # -------Start Routine "translation"-------
+    while continueRoutine:
+        # get current time
+        t = translationClock.getTime()
+        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+        # update/draw components on each frame
+        
+        # *text_7* updates
+        if t >= 0.0 and text_7.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            text_7.tStart = t
+            text_7.frameNStart = frameN  # exact frame index
+            text_7.setAutoDraw(True)
+        
+        # *key_resp_7* updates
+        if t >= 0.0 and key_resp_7.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            key_resp_7.tStart = t
+            key_resp_7.frameNStart = frameN  # exact frame index
+            key_resp_7.status = STARTED
+            # keyboard checking is just starting
+            win.callOnFlip(key_resp_7.clock.reset)  # t=0 on next screen flip
+            event.clearEvents(eventType='keyboard')
+        if key_resp_7.status == STARTED:
+            theseKeys = event.getKeys(keyList=['return'])
+            
+            # check for quit:
+            if "escape" in theseKeys:
+                endExpNow = True
+            if len(theseKeys) > 0:  # at least one key was pressed
+                key_resp_7.keys = theseKeys[-1]  # just the last key pressed
+                key_resp_7.rt = key_resp_7.clock.getTime()
+                # a response ends the routine
+                continueRoutine = False
+        
+        # check if all components have finished
+        if not continueRoutine:  # a component has requested a forced-end of Routine
+            break
+        continueRoutine = False  # will revert to True if at least one component still running
+        for thisComponent in translationComponents:
+            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                continueRoutine = True
+                break  # at least one component has not yet finished
+        
+        # check for quit (the Esc key)
+        if endExpNow or event.getKeys(keyList=["escape"]):
+            core.quit()
+        
+        # refresh the screen
+        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+            win.flip()
+    
+    # -------Ending Routine "translation"-------
+    for thisComponent in translationComponents:
+        if hasattr(thisComponent, "setAutoDraw"):
+            thisComponent.setAutoDraw(False)
+    # check responses
+    if key_resp_7.keys in ['', [], None]:  # No response was made
+        key_resp_7.keys=None
+    trials.addData('key_resp_7.keys',key_resp_7.keys)
+    if key_resp_7.keys != None:  # we had a response
+        trials.addData('key_resp_7.rt', key_resp_7.rt)
+    # the Routine "translation" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset()
     
     # ------Prepare to start Routine "blank_long"-------
     t = 0
