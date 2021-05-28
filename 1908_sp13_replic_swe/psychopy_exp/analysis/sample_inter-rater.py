b183c39568e90a5ce862c1ade2036db48926b048
montero-melis <montero.gui@gmail.com>
2021-05-28 14:53:23 +0200

    Complete script to copy random sample of sound files for interrater agreement test

--- 1908_sp13_replic_swe/psychopy_exp/analysis/sample_inter-rater.py
+++ 1908_sp13_replic_swe/psychopy_exp/analysis/sample_inter-rater.py
@@ -4,19 +4,25 @@ import pandas
 import re
 import os
 import random
+import shutil
 
+# participant metadata with RA comments
 df_ppts = pandas.read_csv("../participant_data_210526.csv")
 valid_id = df_ppts.iloc[:, 1] <= 2  # values of 1 and 2 denote valid participants
 df_ppts = df_ppts[valid_id]
 
+# extract valid participants' IDs
 ids_valid = []
 for id in df_ppts["pptID"].tolist():
     m = re.search("^(\d+),.*", id)
     ids_valid.append(m.group(1))
 print(ids_valid)
 
+# all participant files generated from psychopy; this include invalid participants, either because they are from pilot
+# or bc participant has to be excluded, in which case it's marked with "_excl" at the end of the filename)
 all_files = os.listdir("../data")
 
+# make the selection of valid files
 target_files = []
 excluded_files = []  # to verify what we're excluding
 
@@ -29,13 +35,18 @@ for f in all_files:
             excluded_files.append(mymatch)
         else:
             for valid in ids_valid:
-                m3 = re.match("^" + valid + "_", mymatch)
+                m3 = re.match("^" + valid + "_", mymatch)  # check filename corresponds to a valid ID
                 if m3:
                     target_files.append(mymatch)
 
+print("\ntarget files:")
 print(target_files)
-print(excluded_files)
+print("how many:")
 print(len(target_files))
+
+print("\nexcluded files:")
+print(excluded_files)
+print("how many:")
 print(len(excluded_files))
 
 # Now get all sound files from target participants
@@ -50,6 +61,15 @@ for f in os.listdir("../sound_files"):
 
 # Select a random sample of 5% of all trials
 intended_length = round(.05 * len(sound_files))
-random.seed(644785366)
+print('There are %s target sound files in total; we select %s (5%%) of them.' % (len(sound_files), intended_length))
+random.seed(64478536)
 sound_files_sample = random.sample(sound_files, k=intended_length)
-print(sound_files_sample)
+
+# copy them to a separate directory for coding
+path_source = "../sound_files/"
+path_target = "../sound_files_interrater_agreement/"
+if not os.path.exists(path_target):
+    os.mkdir(path_target)
+for f in sound_files_sample:
+    src_path = path_source + f
+    shutil.copy(src_path, path_target)
