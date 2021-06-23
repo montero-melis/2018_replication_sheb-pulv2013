ab431dd36a166d6a5820427f3160d8bce86fabff
montero-melis <montero.gui@gmail.com>
2021-06-23 16:15:56 +0200

    Analysis60 (#63)
    Includes analysis script and associated functions to analyze data according to our pre-registered analysis pipeline.
    
    Commit messages (squashed):
    
    * Factors the functions to extract data into separate script so functions can be used in other scripts as well
    
    * Deletes "_1" at end of filename which seemed to be there by mistake (checked with Petrus)
    
    * Adds a missing transcription after checking with Petrus (had been deleted)
    
    * Renames old participant data file
    
    * Adds most recent version of participant data file from LabFolder (210609)
    
    * Adds some code to save to disk the excluded trials due to paradiddle performance - and the actual file
    
    * Adds script to get all the valid data from the first 60 participants (for analysis) - and saves the data file to disk
    
    * First version of analysis script and associated script with functions
    
    * Updates the code to plot for a good-looking figure; contains the code to fit the model (and does so successfully), but BF is not working yet.
    
    * The script now analyzes the data using both the normal BF and the replication BF. Note that some code snippets are commented out because model fitting takes very long

---
+++ 1908_sp13_replic_swe/psychopy_exp/analysis/myfunctions/analysis_fncs.R
@@ -0,0 +1,250 @@
+## Functions used to analyze the data following our Registered Report
+## (see Appendix E in https://osf.io/26fbh/)
+
+
+###########################################################
+## Global parameters etc. to be used by other functions or script
+###########################################################
+
+# ensure target directory for saving figures exists
+my_figs_path <- "myfigures/"
+dir.create(my_figs_path)
+
+# specify certain plotting parameters to be used as defaults
+my_dodge_w <- .2
+my_jitter_w <- .2
+my_alpha <- .3
+my_dotsize <- 1
+my_linesize <- 1
+
+
+###########################################################
+## General data manipulation or convenience functions
+###########################################################
+
+# save ggplot with global specifications
+my_ggsave <- function(
+  fig_name,
+  mywidth  = 4,
+  myheight = 4,
+  myplot = last_plot(),
+  type = ".png",
+  target_folder = my_figs_path
+  ) {
+  fname <- paste0(target_folder, fig_name, type)
+  ggsave(filename = fname, plot = myplot, width = mywidth, height = myheight)
+}
+
+
+###########################################################
+## Plots
+###########################################################
+
+# Plot raw data
+plot_raw_data <- function(df, excl_CONTROL = TRUE) {
+
+  if (excl_CONTROL) {
+    df <- df %>% filter(block_type != "CONTROL")
+  }
+
+  # compute subject means
+  df_sbj <- df %>%
+    group_by(subject, block_type, word_type) %>%
+    summarise(prop_error = mean(error)) %>%
+    mutate(block_type = as.factor(block_type))
+
+  # basic plot
+  p <- ggplot(
+    data = df_sbj,
+    aes(x = block_type, y = prop_error, color = word_type, shape = word_type)
+    ) +
+    geom_point(
+      alpha = my_alpha,
+      position = position_jitterdodge(
+        jitter.width = my_jitter_w, jitter.height = 0, dodge.width = my_dodge_w,
+        seed = 6477
+        )
+      ) +
+    stat_summary(
+      fun.data = "mean_cl_boot", position = position_dodge(width = my_dodge_w),
+      size = my_dotsize
+    ) +
+    xlab("Block type") +
+    ylab("Proportion of errors") +
+    labs(color = "word type", shape = "word type") +
+    guides(color = guide_legend(override.aes = list(size = .75)))
+  # add lines if CONTROL is excluded
+  if (excl_CONTROL) {
+    p <- p +
+      stat_summary(
+        aes(x = as.numeric(block_type)),
+        fun = mean, 
+        geom = "line",
+        position = position_dodge(width = my_dodge_w),
+        size = my_linesize
+        )
+  }
+
+  p
+}
+
+
+
+###########################################################
+## Analysis pipeline
+###########################################################
+
+# Create a function that streamlines data processing and model fitting:
+analysis_pipe <- function(
+  df,             # expects data frame in a specific format
+  repBF = FALSE,  # compute replication BF?
+  # if repBF=TRUE, repBF_post should be a vector of 2 values, consisting of
+  # the mean and SD of the posterior for the critical interaction effect
+  # based on the re-analysis of the original data
+  repBF_post = NULL  
+  ) {
+  
+  # to be used for filename
+  df_name <- deparse(substitute(df))
+  if (repBF) { df_name <- paste(df_name, "repBF", sep = "_") }
+  # name of the file where results are saved (or have been saved if run earlier)
+  file_name <- paste("results_brms_", df_name, ".rds", sep ="")
+
+  # If model list has been saved to disk, load it and **RETURN**,
+  # otherwise fit the models
+  if (file.exists(file_name)) {
+    cat("The list of models", file_name, "exists on disk, so we load it...\n\n")
+    return(readRDS(file_name))
+  } else {
+    cat("The list of models", file_name, "doesn't exist on disk, so we will fit it.",
+        "\nThis will take some time. Don't let your computer go to sleep!\n\n")    
+  }
+
+  # coding scheme: contrast code factors and standardize numerical predictors
+  cat("Coding scheme:\n\n")
+  df$block_type <- factor(df$block_type)
+  contrasts(df$block_type) <- contr.sum(2)
+  colnames(contrasts(df$block_type)) <- "arm_vs_leg"
+  print("df$block_type")
+  print(contrasts(df$block_type))
+  cat("\n")
+  
+  df$word_type <- factor(df$word_type)
+  contrasts(df$word_type) <- contr.sum(2)
+  colnames(contrasts(df$word_type)) <- "arm_vs_leg"
+  print("df$word_type")
+  print(contrasts(df$word_type))
+  cat("\n")
+  
+  df$trial_exp_z <- scale(df$trial_exp)
+  df$word_pos_z <- scale(df$word_pos)
+  df$preced_error_z <- scale(df$preced_error)
+  
+  # Formula of the full model:
+  formula_full <- as.formula(paste(
+    "error ~",  # DV
+    "1 + block_type * word_type +",  # critical manipulations and interaction
+    "trial_exp_z + word_pos_z + preced_error_z +",  # nuisance predictors
+    "(1 + block_type * word_type | subject) + (1 + block_type | verb)"  # maximal random structure
+  ))
+  cat("FULL model formula:\n\n")
+  print(formula_full)
+  # Formula of the null model (remove the population-level interaction)
+  cat("\n\nNULL model formula:\n\n")
+  formula_null <- update(formula_full, ~ . - block_type : word_type)
+  print(formula_null)
+  
+  # Priors:
+  # As default, specify weakly informative priors: N(0,sigma^2 = 4) for
+  # population-level (fixed) effects
+  # NB: In Stan a normal distribution is specified with sigma (*not* sigma^2), see
+  # https://mc-stan.org/docs/2_18/functions-reference/normal-distribution.html
+  # and
+  # https://stackoverflow.com/questions/52893379/stan-in-r-standard-deviation-or-variance-in-normal-distribution
+
+  # Print to screen the priors that can be specified for full and null models:
+  cat("\nThe following priors can be specified for the FULL model:\n\n")
+  print(get_prior(formula_full, df))
+  cat("\nThe following priors can be specified for the NULL model:\n\n")
+  print(get_prior(formula_null, df))
+
+  # Set weakly informative priors by default
+  myprior <- set_prior("normal(0, 2)", class = "b")  
+  # But if repBF=T, then replace prior for critical interaction with values
+  # from the posterior of our SP13 re-analysis:
+  if (repBF) {
+  myprior_repBF <- c(
+    myprior,
+    set_prior(
+      paste("normal(", repBF_post[1], ",", repBF_post[2], ")", sep = ""),
+      class = "b",
+      coef = "block_typearms_vs_legs:word_typearms_vs_legs"
+      )
+    ) 
+  }
+
+  # Print priors to screen
+  cat("\nThe following priors will be used (NB: repBF = ", repBF, "):\n\n", sep = "")
+  if (repBF) {
+    print(myprior_repBF)
+  } else {
+    print(myprior)
+  }
+
+  # fit null model (without interaction):
+  # NB: The prior for interaction is not defined in the null model:
+  bfm_null <- brm(
+    formula = formula_null,
+    data = df,
+    prior = myprior,
+    family = "bernoulli",
+    iter = 20000, warmup = 2000, chains = 4,  # https://discourse.mc-stan.org/t/bayes-factor-using-brms/4469/3
+    save_all_pars = TRUE  # necessary for brms::bayes_factor() later
+  )
+  # Model *without* interaction
+  bfm_full <- update(
+    bfm_null,
+    formula = formula_full,
+    prior = if (repBF) { myprior_repBF } else { myprior },
+    )
+
+  # pack models and data into a list and give sensible names to each object
+  out <- list(bfm_full, bfm_null, df)
+  names(out) <- paste(df_name, c("bfm_full", "bfm_null", "dataset"), sep = "_")
+
+  # save list to disk:
+  saveRDS(out, file = file_name)
+  
+  out
+}
+
+
+# A wrapper for our BF analysis such that the Bayes factor analysis with bridge
+# sampling is only run if it is not already present on disk:
+my_BF_wrapper <- function(
+  model_list  # should be a list as output by analysis_pipe()
+  ) {
+
+  # to be used for filename
+  model_list_name <- deparse(substitute(model_list))
+  # name of the file where results are saved (or have been saved if run earlier)
+  file_name <- paste(model_list_name, "_BF.rds", sep ="")
+  print(file_name)
+
+  # If the BF has already been saved to disk, load it, otherwise run it:
+  if (file.exists(file_name)) {
+    cat("The BF analysis", file_name, "exists on disk, so we load it...\n\n")
+    return(readRDS(file_name))
+  } else {
+    cat("The BF analysis", file_name, "doesn't exist on disk, so we will run it.",
+        "\nThis may take some time (30+ min).\n\n")    
+  }
+
+  # Run the BF analysis with bridge sampling
+  BF <- brms::bayes_factor(model_list[[1]], model_list[[2]])
+  # save it to disk
+  saveRDS(BF, file = file_name)
+  # return it
+  BF
+}
+
